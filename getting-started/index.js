23b5ac8a64a9fe2d21264c8535577703b367c07d
Gaurav <Gaurav.techgeek@gmail.com>
2019-07-27 20:54:25 +0530

    Create index.js

---
+++ getting-started/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+import SayHello from './components';
+
+ReactDOM.render(<SayHello name={'Code.Learn'}/>, document.querySelector('#app-root'));
